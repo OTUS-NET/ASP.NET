a2bfcea5d8e9256e2177ccb7f7b011f6555f88a5
AATerekhov <A.A.Terekhov@mail.ru>
2024-09-22 22:02:08 +0300

    add mogration

--- EF/src/add migration.bat
+++ EF/src/add migration.bat
@@ -1,4 +1,4 @@
 ﻿
-dotnet ef migrations add FirstDownload --startup-project PromoCodeFactory.WebHost/PromoCodeFactory.WebHost.csproj --project PromoCodeFactory.EntityFramework\PromoCodeFactory.EntityFramework.csproj --context DataContext
+dotnet ef migrations add Second --startup-project PromoCodeFactory.WebHost/PromoCodeFactory.WebHost.csproj --project PromoCodeFactory.EntityFramework\PromoCodeFactory.EntityFramework.csproj --context DataContext
 
 PAUSE
