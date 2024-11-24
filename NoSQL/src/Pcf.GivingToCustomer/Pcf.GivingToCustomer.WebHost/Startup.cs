68812c26eb109b97ac6988859472c1c3f03c3278
AATerekhov <A.A.Terekhov@mail.ru>
2024-11-24 07:47:16 +0300

    The beginning of the microservice preferences directory.

--- NoSQL/src/Pcf.GivingToCustomer/Pcf.GivingToCustomer.WebHost/Startup.cs
+++ NoSQL/src/Pcf.GivingToCustomer/Pcf.GivingToCustomer.WebHost/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Castle.Core.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
 using Pcf.GivingToCustomer.Core.Abstractions.Gateways;
 using Pcf.GivingToCustomer.Core.Abstractions.Repositories;
 using Pcf.GivingToCustomer.DataAccess;
