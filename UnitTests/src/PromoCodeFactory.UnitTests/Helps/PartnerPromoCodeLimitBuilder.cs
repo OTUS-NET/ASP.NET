cff9a986bdb4b9e2e7c3d06a1e14d430bc073ad2
AATerekhov <A.A.Terekhov@mail.ru>
2025-01-12 14:28:44 +0300

    test1

--- UnitTests/src/PromoCodeFactory.UnitTests/Helps/PartnerPromoCodeLimitBuilder.cs
+++ UnitTests/src/PromoCodeFactory.UnitTests/Helps/PartnerPromoCodeLimitBuilder.cs
@@ -13,6 +13,7 @@ namespace PromoCodeFactory.UnitTests.Helps
         public object Create(object request, ISpecimenContext context) => Create();       
         public object Create() 
         {
+
             var currentDate = DateTime.UtcNow;
             currentDate.AddDays(new Random().Next(100));
             var result = new SetPartnerPromoCodeLimitRequest()
