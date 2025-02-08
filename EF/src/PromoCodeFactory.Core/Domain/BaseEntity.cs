528a9b1d5a1f53be1db581692f8b3d2e5aeaa05a
Dmitry Ponomar <mitrybox@gmail.com>
2025-02-08 15:59:18 +0300

    BaseEntity modified

--- EF/src/PromoCodeFactory.Core/Domain/BaseEntity.cs
+++ EF/src/PromoCodeFactory.Core/Domain/BaseEntity.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using PromoCodeFactory.Core.Domain.Base;
+using System;
 
 namespace PromoCodeFactory.Core.Domain
 {
-    public class BaseEntity
+    //public class BaseEntity
+    //{
+    //    public Guid Id { get; set; }
+    //}
+    public abstract class BaseEntity : IEntity<Guid>
     {
         public Guid Id { get; set; }
     }
