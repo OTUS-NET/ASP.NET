c7fe7cd425c66748f9ac448af70cf5bb0e834b18
Alexandr Kan <kanale10@yandex.ru>
2025-02-16 15:36:53 +0300

    fix temp state

---
+++ Homeworks/Base/src/PromoCodeFactory.DataAccess/DataContext/PromoCodeContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PromoCodeFactory.Core.Domain.Administration;
+using System;
+
+namespace PromoCodeFactory.DataAccess.DataContext;
+public class PromoCodeContext : DbContext
+{
+    public DbSet<Employee> Employee { get; set; }
+    public DbSet<Role> Role { get; set; }
+
+    public string DbPath { get; }
+
+    public PromoCodeContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "promocode.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
---
+++ Homeworks/Base/src/PromoCodeFactory.DataAccess/Repositories/EfRepository.cs
@@ -0,0 +1,38 @@
+﻿using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromoCodeFactory.DataAccess.Repositories
+{
+    public class EfRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
