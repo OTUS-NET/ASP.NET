2ea64b35baf9b10cab3479aea092a3fc298daa84
Dmitry Ponomar <mitrybox@gmail.com>
2025-03-15 22:15:04 +0300

    EmployeesInMemoryRepository added

---
+++ EF/src/PromoCodeFactory.DataAccess/EmployeesInMemoryRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.Administration;
+using PromoCodeFactory.DataAccess.Repositories;
+
+namespace PromoCodeFactory.DataAccess
+{
+    public class EmployeesInMemoryRepository : InMemoryRepository<Employee>, IEmployeesRepository
+    {
+        public EmployeesInMemoryRepository(IEnumerable<Employee> data) : base(data)
+        {
+        }
+        public Task<Employee> UpdateAsync(Guid id, Employee employee)
+        {
+            Monitor.Enter(lockObj);
+
+            Employee updateEmployee = Data.First(x => x.Id == id);
+            updateEmployee.FirstName = employee.FirstName;
+            updateEmployee.LastName = employee.LastName;
+            updateEmployee.Email = employee.Email;
+
+            if (employee.Roles.Count != 0) updateEmployee.Roles = employee.Roles;
+            updateEmployee.AppliedPromocodesCount = employee.AppliedPromocodesCount;
+
+            Monitor.Exit(lockObj);
+
+            return Task.FromResult(updateEmployee);
+        }
+    }
+}
