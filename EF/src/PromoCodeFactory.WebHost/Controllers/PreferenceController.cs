6217d30413208bc3de5adf01a8e8413e5cb13b8f
Dmitry Ponomar <mitrybox@gmail.com>
2025-03-16 23:14:34 +0300

    PreferenceController added

---
+++ EF/src/PromoCodeFactory.WebHost/Controllers/PreferenceController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PromoCodeFactory.WebHost.Models.Response;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+
+namespace PromoCodeFactory.WebHost.Controllers
+{
+    /// <summary>
+    /// Предпочтения покупателей.
+    /// </summary>
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PreferenceController(IRepository<Preference, Guid> preferenceRepository, IMapper mapper) : ControllerBase
+    {
+        /// <summary>
+        ///  Получить все предпочтения клиентов.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<PreferenceResponse>), 200)]
+        public async Task<IEnumerable<PreferenceResponse>> GetPreferenceAsync() =>
+            (await preferenceRepository.GetAllAsync()).Select(mapper.Map<PreferenceResponse>);
+
+    }
+}
+}
