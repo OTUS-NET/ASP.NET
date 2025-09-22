401266cf744afd80c88ba5f38fcac00b321aa6e9
IGEG <igor127@bk.ru>
2025-09-22 20:19:35 +0300

    test

--- Homeworks/UnitTests/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/CancelPartnerPromoCodeLimitAsyncTests.cs
+++ Homeworks/UnitTests/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/CancelPartnerPromoCodeLimitAsyncTests.cs
@@ -30,7 +30,7 @@ namespace PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
             var partner = new Partner()
             {
                 Id = Guid.Parse("7d994823-8226-4273-b063-1a95f3cc1df8"),
-                Name = "Суперигрушки",
+                Name = "Суперигрушки2",
                 IsActive = true,
                 PartnerLimits = new List<PartnerPromoCodeLimit>()
                 {
