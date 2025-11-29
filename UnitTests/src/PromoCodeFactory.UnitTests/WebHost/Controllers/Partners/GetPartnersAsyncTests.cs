b8b69c9589a48c5f47fed2c45a950a2e058a2143
Dmitry Ponomar <mitrybox@gmail.com>
2025-11-29 19:09:53 +0300

    GetPartnersAsyncTests added

---
+++ UnitTests/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/GetPartnersAsyncTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.UnitTests.Helps;
+using PromoCodeFactory.WebHost.Controllers;
+
+
+namespace PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
+{
+    public class GetPartnersAsyncTests
+    {
+        [Theory, AutoMoqData]
+        public async Task GetPartnersAsync_GettingCollectionOfPartners_ReturnsOk(IEnumerable<Partner> partners, [Frozen] Mock<IPartnerRepository> partnerRepositoryMock, PartnersController partnersController)
+        {
+            //Arrange
+            partnerRepositoryMock.Setup(repo => repo.GetAllAsync()).ReturnsAsync(partners);
+            //Act
+            var result = await partnersController.GetPartnersAsync();
+            //Assert
+            result.Result.Should().BeAssignableTo<OkObjectResult>();
+        }
+    }
+}
