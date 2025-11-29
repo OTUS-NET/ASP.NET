b02ee33fdebc760bb239e458de3dd96c2d529fc3
Dmitry Ponomar <mitrybox@gmail.com>
2025-11-29 18:32:50 +0300

    PromoCodeFactory.Compose.Configuration added

--- EF/src/add migration.bat
+++ EF/src/add migration.bat
@@ -1,4 +1,7 @@
 ﻿
 dotnet ef migrations add Initial --startup-project PromoCodeFactory.WebHost/PromoCodeFactory.WebHost.csproj --project PromoCodeFactory.EntityFramework\PromoCodeFactory.EntityFramework.csproj --context DataContext
 
+dotnet ef database update --startup-project PromoCodeFactory.WebHost/PromoCodeFactory.WebHost.csproj --project PromoCodeFactory.EntityFramework\PromoCodeFactory.EntityFramework.csproj --context DataContext
+
+
 PAUSE
