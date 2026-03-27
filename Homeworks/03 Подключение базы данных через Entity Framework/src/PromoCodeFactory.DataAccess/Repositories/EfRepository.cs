31f31e101cd91189f89d92575b7f10a5388408a8
Petrov Andrej <petrov.andrej.sergeevich@ya.ru>
2026-03-27 06:57:53 +0300

    Реализовал методы в EfRepository.cs

--- Homeworks/03 Подключение базы данных через Entity Framework/src/PromoCodeFactory.DataAccess/Repositories/EfRepository.cs
+++ Homeworks/03 Подключение базы данных через Entity Framework/src/PromoCodeFactory.DataAccess/Repositories/EfRepository.cs
@@ -1,4 +1,5 @@
 using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 using PromoCodeFactory.Core.Abstractions.Repositories;
 using PromoCodeFactory.Core.Domain;
 
@@ -8,39 +9,55 @@ internal class EfRepository<T>(PromoCodeFactoryDbContext context) : IRepository<
 {
     protected virtual IQueryable<T> ApplyIncludes(IQueryable<T> query) => query;
 
-    public Task Add(T entity, CancellationToken ct)
+    public async Task Add(T entity, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        context.Set<T>().Add(entity);
+        await context.SaveChangesAsync(ct);
     }
 
-    public Task Delete(Guid id, CancellationToken ct)
+    public async Task Delete(Guid id, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        var entity = await context.Set<T>().Where(e => e.Id == id).FirstAsync(ct);
+        context.Set<T>().Remove(entity);
+        await context.SaveChangesAsync();
     }
 
-    public Task<IReadOnlyCollection<T>> GetAll(bool withIncludes = false, CancellationToken ct = default)
+    public async Task<IReadOnlyCollection<T>> GetAll(bool withIncludes = false, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        var query = context.Set<T>().AsNoTracking();
+        if (withIncludes)
+            query = this.ApplyIncludes(query);
+        return await query.ToArrayAsync(ct);
     }
 
-    public Task<T?> GetById(Guid id, bool withIncludes = false, CancellationToken ct = default)
+    public async Task<T?> GetById(Guid id, bool withIncludes = false, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        var query = context.Set<T>().AsNoTracking();
+        if (withIncludes)
+            query = this.ApplyIncludes(query);
+        return await query.FirstOrDefaultAsync(e => e.Id == id, ct);
     }
 
-    public Task<IReadOnlyCollection<T>> GetByRangeId(IEnumerable<Guid> ids, bool withIncludes = false, CancellationToken ct = default)
+    public async Task<IReadOnlyCollection<T>> GetByRangeId(IEnumerable<Guid> ids, bool withIncludes = false, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        var query = context.Set<T>().AsNoTracking().IntersectBy(ids, e => e.Id);
+        if (withIncludes)
+            query = this.ApplyIncludes(query);
+        return await query.ToArrayAsync(ct);
     }
 
-    public Task<IReadOnlyCollection<T>> GetWhere(Expression<Func<T, bool>> predicate, bool withIncludes = false, CancellationToken ct = default)
+    public async Task<IReadOnlyCollection<T>> GetWhere(Expression<Func<T, bool>> predicate, bool withIncludes = false, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        var query = context.Set<T>().AsNoTracking().Where(predicate);
+        if (withIncludes)
+            query = this.ApplyIncludes(query);
+        return await query.ToArrayAsync(ct);
     }
 
-    public Task Update(T entity, CancellationToken ct)
+    public async Task Update(T entity, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        context.Set<T>().Update(entity);
+        await context.SaveChangesAsync(ct);
     }
 
 }
